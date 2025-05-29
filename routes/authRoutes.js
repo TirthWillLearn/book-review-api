8bb51ad9eee175163bce0debc970b6952e8e08af
Tirth Patel <111308497+TirthWillLearn@users.noreply.github.com>
2025-05-29 19:09:30 +0530

    authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,15 @@
+// Importing necessary modules
 const express = require("express");
-const router = express.Router();
-const { signup, login } = require("../controllers/authController");
+const router = express.Router();  // Create an Express router to handle routes
+const { signup, login } = require("../controllers/authController");  // Importing signup and login functions from authController
 
+// Route for user signup
+// When a POST request is made to '/signup', the signup function will be executed
 router.post("/signup", signup);
+
+// Route for user login
+// When a POST request is made to '/login', the login function will be executed
 router.post("/login", login);
 
+// Export the router so it can be used in the main app file
 module.exports = router;
